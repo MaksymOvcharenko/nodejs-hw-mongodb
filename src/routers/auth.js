1b1440ac047f4d4bbac7b03e0a9403b4dab6ccd7
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-10-03 10:37:28 +0200

    refres + middleware authf

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -5,6 +5,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   loginUserController,
   logoutUserController,
+  refreshUserSessionController,
   registerUserController,
 } from '../controllers/auth.js';
 
@@ -23,5 +24,10 @@ router.post(
   '/logout',
 
   ctrlWrapper(logoutUserController),
+);
+router.post(
+  '/refresh',
+
+  ctrlWrapper(refreshUserSessionController),
 );
 export default router;

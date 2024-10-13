0cb84f8234c47102c88ceb7b976903ae57e307df
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-10-13 17:46:55 +0200

    Swagger

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import routers from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandlerMiddleware } from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
@@ -31,6 +32,7 @@ export const setupServer = () => {
     });
   });
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(routers);
   app.use('*', notFoundHandler);
   app.use(errorHandlerMiddleware);

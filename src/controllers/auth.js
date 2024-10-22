ad28608b83241af9c2d35dd7d15c02c71217e9e8
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-10-22 23:21:32 +0200

    туц

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -53,10 +53,12 @@ export const logoutUserController = async (req, res) => {
 const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
+    sameSite: 'None',
     expires: new Date(Date.now() + ONE_DAY),
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
+    sameSite: 'None',
     expires: new Date(Date.now() + ONE_DAY),
   });
 };
